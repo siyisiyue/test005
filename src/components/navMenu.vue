336c7cfc591b8d2d1a2c9fa50cd55b2f98619298
jie <siyisiyue@qq.com>
2020-12-22 10:48:20 +0800

    222

--- src/components/navMenu.vue
+++ src/components/navMenu.vue
@@ -17,7 +17,7 @@
           <i class="el-icon-s-tools"></i>
           <span>Vuex</span>
         </template>
-        <el-menu-item index="/vuex/jishuqi">计数器修改</el-menu-item>
+        <el-menu-item index="/vuex/jishuqi">计数器修改2</el-menu-item>
         <el-submenu index="2-2">
           <template slot="title">选项4</template>
           <el-menu-item index="2-2-1">选项1</el-menu-item>
